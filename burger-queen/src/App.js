eb353482fd48c3270ea1e03c1d0b04310cc507dc
silvanarl <ramossilvana495@gmail.com>
2020-07-17 10:26:26 -0500

    inicializando React

---
+++ burger-queen/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './title-burger-queen.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.figma.com/file/zZ4K4yKr939l881WJY3LuU/Portafolio?node-id=0%3A1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Silvana esta aquí
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
