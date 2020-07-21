bf90b2e4b67204689eef186ec796d57eea6271af
IsabelaSanchez <natyst_26@hotmail.com>
2020-07-21 12:21:43 -0500

    Changing CSS first page

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import logo from './assets/title-burger-queen.svg';
+import slogan from './assets/burger-queen-slogan.svg';
+import waiter from './assets/waiter.svg';
+import chef from './assets/chef-hat.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const ButtonInit = ({ value }) => (
+  <button type="button" className="ButtonInit">
+    { value }
+  </button>
+);
+const ContainerButton = () => (
+  <div className="container-button">Hola!</div>
+);
+const CompButtonOrders = () => (
+  <div className="container-button">
+    <img src={waiter} className="comp-waiter" alt="waiter" />
+    <ButtonInit value="Pedidos" />
+  </div>
+);
+const CompButtonKitchen = () => (
+  <div className="">
+    <img src={chef} className="comp-kitchen" alt="kitchen" />
+    <ButtonInit value="Cocina" />
+  </div>
+);
+const App = () => (
+  <div className="App-background">
+    <div className="App-background-inside">
+      <header className="App-header relative">
+        <img src={logo} className="App-logo relative" alt="logo" />
+        <img src={slogan} className="App-slogan relative" alt="slogan" />
       </header>
+      <main>
+        <ContainerButton />
+        <CompButtonOrders />
+        <CompButtonKitchen />
+      </main>
     </div>
-  );
-}
-
+  </div>
+);
+ButtonInit.propTypes = {
+  value: PropTypes.string.isRequired,
+};
 export default App;
