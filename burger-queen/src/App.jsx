d45c665f890635eb4573d607153fcf08731e4dd8
IsabelaSanchez <natyst_26@hotmail.com>
2020-07-23 10:14:58 -0500

    Soy un commit

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import Home from './Components/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import PlaceAndViewOrders from './components/waiter-choice';
+import PlaceAndViewOrders from './Components/Waiter-choice';
 
 const App = () => (
   <div className="">
