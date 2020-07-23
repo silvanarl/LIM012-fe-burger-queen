496c75300f53dd9022a21cd39973f17e1890e714
IsabelaSanchez <natyst_26@hotmail.com>
2020-07-23 09:39:43 -0500

    la vida es fea

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import Home from './Components/Home';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import PlaceAndViewOrders from './Components/Waiter-choice';
+import PlaceAndViewOrders from './components/waiter-choice';
 
 const App = () => (
   <div className="">
