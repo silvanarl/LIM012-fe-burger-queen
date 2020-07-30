b7a05f1cac5b1fcfd64821f4fb8b38b8b12c67da
silvanarl <ramossilvana495@gmail.com>
2020-07-29 23:05:41 -0500

    tests para probar renderizado usando enzyme shallow

--- burger-queen/src/App.test.js
+++ burger-queen/src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import Home from './Components/Home';
+import Waiter from './Components/Waiter-choice';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders PEDIDOS at button', () => {
+  const { getByText } = render(<Home />);
+  const buttonElement = getByText(/PEDIDOS/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('render COCINA at button', () => {
+  const { getByText } = render(<Home />);
+  const buttonElement = getByText(/COCINA/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+
+test('App renderiza sin problemas', () => {
+  shallow(<App />);
+});
+
+test('Home renderiza', () => {
+  shallow(<Home />);
+});
+
+test('Waiter-choice renderiza', () => {
+  shallow(<Waiter />);
+});
+
+test('Bienvenida no existe', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to my ReactApp!</h2>;
+  expect(wrapper.contains(welcome)).toEqual(false);
 });
