257a99c14251bf154a1ae5ef9b81ce8eda076f8d
silvanarl <ramossilvana495@gmail.com>
2020-08-03 11:28:11 -0500

    vista de menu

---
+++ burger-queen/src/Components/Do-orders.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Background from './Background';
+import '../App.css';
+
+const DoOrders = () => (
+  <div>
+    <Background />
+    <h1>Hola</h1>
+  </div>
+);
+export default DoOrders;
