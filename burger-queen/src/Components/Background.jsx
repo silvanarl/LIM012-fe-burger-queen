cff1fede64de4bac51280bc278c0fbd302de9899
silvanarl <ramossilvana495@gmail.com>
2020-08-04 18:23:01 -0500

    background sin retorno

--- burger-queen/src/Components/Background.jsx
+++ burger-queen/src/Components/Background.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import back from '../assets/arrowBack.svg';
 import logo from '../assets/title-burger-queen.svg';
 
 const Background = () => (
   <div>
     <header className="headerMain flexRow">
-      <div className="start">
-        <a href="/home"><img src={back} alt="Volver" /></a>
-      </div>
       <div className="center">
         <img src={logo} alt="logo" />
       </div>
