a08fa5e53dda5283d2a6133292906187def5f15b
IsabelaSanchez <natyst_26@hotmail.com>
2020-08-05 00:27:37 -0500

    finally fixed

--- burger-queen/src/Components/Do-orders.jsx
+++ burger-queen/src/Components/Do-orders.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import Background from './Background';
+import { ButtonMenu, ButtonBreakfast, ButtonDrinks } from './Button-foodType';
 import '../css/app.css';
 
 const DoOrders = () => (
   <div>
     <Background />
     <div className="flexRow">
-      <div>
-        <div className="containerOptionFood" />
-        <div className="containerOptionFood" />
-        <div className="containerOptionFood" />
-        <div className="containerAllFood flexRow" />
-      </div>
-      <div className="containerFolder flexRow">
-        <div className="folder" />
-        <div className="folderTwo" />
-      </div>
+      <ButtonBreakfast />
+      <ButtonMenu />
+      <ButtonDrinks />
+    </div>
+    <div className="containerAllFood flexRow" />
+    <div className="containerFolder flexRow">
+      <div className="folder" />
+      <div className="folderTwo" />
     </div>
   </div>
 );
