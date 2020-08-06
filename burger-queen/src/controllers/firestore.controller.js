30f26f282562f0ec0da6440fd198417fe49dc751
silvanarl <ramossilvana495@gmail.com>
2020-08-06 10:54:27 -0500

    firebase agregado desde cli y config

---
+++ burger-queen/src/controllers/firestore.controller.js
@@ -0,0 +1,19 @@
+const firebase = require('firebase/app');
+console.log(firebase);
+// Create a reference to the cities collection
+// const food = firebase.firestore().collection('food');
+
+// Create a query against the collection
+// const foodMenu = food.where('menu', '==', true).get()
+//   .then((snapshot) => {
+//     if (snapshot.empty) {
+//       console.log('No matching documents.');
+//     } else {
+//       snapshot.forEach((doc) => {
+//         console.log(doc.data);
+//       });
+//     }
+//   })
+//   .catch(err => {
+//     console.log('Error getting documents', err);
+//   });
---
+++ burger-queen/src/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWWbCiIGl-6RMTpiNCkDPj90jFcQQnvp8',
+  authDomain: 'burger-queen-16c95.firebaseapp.com',
+  databaseURL: 'https://burger-queen-16c95.firebaseio.com',
+  projectId: 'burger-queen-16c95',
+  storageBucket: 'burger-queen-16c95.appspot.com',
+  messagingSenderId: '248833030673',
+  appId: '1:248833030673:web:ddf6fe7e9b2eca3ef20fb0',
+};
+
+export default firebaseConfig;
