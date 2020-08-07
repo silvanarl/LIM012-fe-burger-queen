09421f0bf35fcf52bb51f89042ff7bd7cddc54ad
silvanarl <ramossilvana495@gmail.com>
2020-08-07 12:57:40 -0500

    -config firebase

--- burger-queen/src/App.jsx
+++ burger-queen/src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import firebase from 'firebase';
-import { firebaseConfig } from './firebase-config';
 import Home from './Components/Home';
 import './css/app.css';
 import PlaceAndViewOrders from './Components/Waiter-choice';
