ec54d805539797aeb09cd298ecd0f189fd7e2a0a
silvanarl <ramossilvana495@gmail.com>
2020-08-10 11:19:13 -0500

    cambio de callback a promesa

--- burger-queen/src/controllers/firestore.controller.js
+++ burger-queen/src/controllers/firestore.controller.js
@@ -3,26 +3,16 @@ import Firebase from '../firebase-config';
 
 const db = Firebase.firestore();
 const food = db.collection('food');
-const getPriceAndNameFood = (callback) => food.where('breakfast', '==', true).get().then((snapshot) => {
+const getPriceAndNameFood = () => food.where('breakfast', '==', true).get().then((snapshot) => {
   const foodBreakfast = [];
-  if (snapshot.empty) {
-    console.log('No matching documents.');
-  } else {
-    snapshot.forEach((doc) => {
-      const breakfastObj = {
-        price: doc.data().price,
-        name: doc.data().name,
-      };
-      foodBreakfast.push(breakfastObj);
-    });
-    console.log(foodBreakfast);
-    console.log(foodBreakfast[0].name);
-    callback(foodBreakfast);
-  }
-  // console.log(foodBreakfast.breakfastObj.price);
-//   console.log(foodBreakfast.find((e) => e > 2));
-//   console.log(foodBreakfast.indexOf('café americano'));
-//   console.log(foodBreakfast.length);
+  snapshot.forEach((doc) => {
+    const breakfastObj = {
+      price: doc.data().price,
+      name: doc.data().name,
+    };
+    foodBreakfast.push(breakfastObj);
+  });
+  return foodBreakfast;
 })
   .catch((err) => {
     console.log('Error getting documents', err);
