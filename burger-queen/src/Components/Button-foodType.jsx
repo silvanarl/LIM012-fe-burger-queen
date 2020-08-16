9bea8c24d883e89e7b344459c8eb201d0876168c
silvanarl <ramossilvana495@gmail.com>
2020-08-16 01:07:54 -0500

    estado de cliente ok, se envia a firebase

--- burger-queen/src/Components/Button-foodType.jsx
+++ burger-queen/src/Components/Button-foodType.jsx
@@ -3,6 +3,9 @@ import '../css/app.css';
 import Menu from '../assets/white-icons/menu-white.svg';
 import Drinks from '../assets/white-icons/drinks-white.svg';
 import Breakfast from '../assets/white-icons/breakfast-white.svg';
+import menuBrown from '../assets/brown-icons/menu-brown.svg';
+import breakfastBrown from '../assets/brown-icons/breakfast-brown.svg';
+import drinksBrown from '../assets/brown-icons/drinks-brown.svg';
 
 const ButtonFoodType = ({ food, icon }) => (
   <div className="flexColumn containerFood">
@@ -11,13 +14,19 @@ const ButtonFoodType = ({ food, icon }) => (
   </div>
 );
 
-export const ButtonBreakfast = () => (
-  <div className="flexColumn">
-    <button type="button" className="containerOptionFood">
-      <ButtonFoodType food="Desayuno" icon={Breakfast} />
-    </button>
-  </div>
-);
+export const ButtonBreakfast = () => {
+  const handleClick = (event) => {
+    const buttonElement = event.target;
+    buttonElement.classList.toggle('toggleClass');
+  };
+  return (
+    <div className="flexColumn">
+      <button type="button" onClick={handleClick} className="containerOptionFood">
+        <ButtonFoodType food="Desayuno" icon={Breakfast} />
+      </button>
+    </div>
+  );
+};
 
 export const ButtonMenu = () => (
   <div className="flexColumn">
